080bbac63d68f41c933ef8872c670465ac37693d
DarkHorse <44453417+allenwsh82@users.noreply.github.com>
2024-08-20 13:55:39 +0800

    Update inference_llama3_8b_bf16_ipex.py

--- inference_llama3_8b_bf16_ipex.py
+++ inference_llama3_8b_bf16_ipex.py
@@ -28,6 +28,11 @@ model = ipex.optimize(model.eval(), dtype=torch.bfloat16, inplace=True, level="O
 
 streamer = TextStreamer(tokenizer)
 
+print("This Demo is Powered by Intel Xeon 5th Gen 8592+ on Intel Tiber Developer Cloud")
+print()
+prompt=input("Please enter your prompt: ")
+print()
+
 messages = [
     {"role": "system", "content": "You are a helpful AI assistant for travel tips and recommendations"},
     {"role": "user", "content": "Help me to plan for a trip to Taiwan"},
